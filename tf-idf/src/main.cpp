98160ed17f679ce5ad4c6a72b126aee275f17586
Afnan Khalid <afnank19@gmail.com>
2025-03-01 18:50:20 +0500

    add query execute function

--- tf-idf/src/main.cpp
+++ tf-idf/src/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 #include "file_utils.hpp"
-// #include "../include/map_utils.hpp"
 #include "Indexer.hpp"
-// #include "../include/VectorMath.hpp"
 #include "QueryRunner.hpp"
+
+// Should probably move this elsewhere, but since main is not that complex, it is fine here
+std::vector<std::pair<std::string, float>> execute_query(std::string query, Indexer indexer, std::pair<std::vector<std::string>, std::vector<std::vector<float>>> documents) {
+    std::vector<std::string> query_tokens = query_tokenizer(query);
+
+    auto tf_vec = indexer.create_tf_vector(query_tokens, query_tokens.size());
+    std::vector<float> query_tf_idf_vec = indexer.create_tf_idf_vector(tf_vec);
+
+    auto search_result = QueryRunner::run_query(query_tf_idf_vec, documents);
+
+    return search_result;
+}
 
 int main() {
     std::cout << "----BEGIN-ENGINE----" << std::endl;
@@ -26,12 +37,8 @@ int main() {
     std::string query;
     std::cout << "ENTER QUERY: ";
     std::getline(std::cin, query);
-    std::vector<std::string> query_tokens = query_tokenizer(query);
 
-    auto tf_vec = indexer.create_tf_vector(query_tokens, query_tokens.size());
-    std::vector<float> query_tf_idf_vec = indexer.create_tf_idf_vector(tf_vec);
-
-    auto search_result = QueryRunner::run_query(query_tf_idf_vec, documents);
+    auto search_result = execute_query(query, indexer, documents);
 
     for (const auto& itr: search_result) {
         std::cout << itr.first << " rel -> " << itr.second << "\n";
